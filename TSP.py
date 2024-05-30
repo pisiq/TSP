33e4bd2e0819869c90a068997d75541e700f35c4
Pisica <77281067+pisiq@users.noreply.github.com>
2024-05-30 20:54:43 +0300

    TSP.py

--- TSP.py
+++ TSP.py
@@ -4,10 +4,23 @@ from Astar import a_star_tsp
 from Generator import generate_random_graph
 
 
-graph_size = int(input("Number of cites:"))
-weight_range = (1, 20)
-graph = generate_random_graph(graph_size, weight_range)
+test_default = bool(int(input("Do you want to test the default graph (1) or generate a random one (0)?")))
+if test_default:
+    graph = [
+        [0, 10, 15, 20],
+        [10, 0, 35, 25],
+        [15, 35, 0, 30],
+        [20, 25, 30, 0]
+    ]
+    for row in graph:
+        print(row)
+else:
+    graph_size = int(input("Number of cites:"))
+    weight_range = (1, 20)
+    graph = generate_random_graph(graph_size, weight_range)
+
 start_city = 0
+
 bfs_path, bfs_cost = bfs_tsp(graph, start_city)
 print(f"BFS Path: {bfs_path}, Cost: {bfs_cost}")
 
